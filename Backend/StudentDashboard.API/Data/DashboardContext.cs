cec949705c075f78f8e7a305eb4f9229968d6749
edengavrilov <gavrilov1701@gmail.com>
2026-02-23 21:25:37 +0200

    Finished Backend infrastructure and Task model

---
+++ Backend/StudentDashboard.API/Data/DashboardContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentDashboard.API.Models;
+using System.Collections.Generic;
+
+namespace StudentDashboard.API.Data
+{
+    public class DashboardContext : DbContext
+    {
+        public DashboardContext(DbContextOptions<DashboardContext> options) : base(options)
+        {
+        }
+
+        public DbSet<StudentTask> StudentTasks { get; set; }
+    }
+}
